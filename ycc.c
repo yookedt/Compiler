02236da0bf47943913094a7eb71a8ed2abdd0103
yookedt <s18138@numazu.kosen-ac.jp>
2021-01-31 12:15:41 +0900

    エラー処理を追加しました

--- ycc.c
+++ ycc.c
@@ -4,7 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+void error_at(char *loc,char *fmt,...);
+//入力プログラム
+char *user_input;
+//エラーの箇所を報告する
+void error_at(char *loc,char *fmt, ...){
+	va_list ap;
+	va_start(ap,fmt);
 
+	int pos = loc - user_input;
+	fprintf(stderr,"%s\n",user_input);
+	fprintf(stderr,"%*s",pos," "); //pos個の空白を出力
+	fprintf(stderr,"^");
+	vfprintf(stderr,fmt,ap);
+	fprintf(stderr,"\n");
+	exit(1);
+}
 //トークンの種類
 typedef enum{
 	TK_RESERVED, //記号
@@ -55,7 +70,7 @@ void expect(char op){
 //それ以外の場合にはエラーを報告する.
 int expect_number(){
 	if(token->kind != TK_NUM) 
-		error("数ではありません");
+		error_at(token->str,"数ではありません");
 	int val = token->val;	
 	token = token->next;
 	return val;
@@ -113,6 +128,8 @@ int main(int argc,char **argv){
 		return 1;
 	}
 
+	user_input =argv[1];	
+
 	//トークナイズする
 	token = tokenize(argv[1]);
 
@@ -140,4 +157,8 @@ int main(int argc,char **argv){
 	printf("  ret\n");
 	return 0;
 } 
+
+
+
+
 	
